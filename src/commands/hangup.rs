ce25674148d850f6030b2550e95a00bc750fbf03
mzh-tbg <hussainmuhammadzain@gmail.com>
2025-04-29 00:17:23 +0100

    Added where hangup command ends the call that is currently waiting

--- src/commands/hangup.rs
+++ src/commands/hangup.rs
@@ -13,19 +13,34 @@ pub async fn hangup(ctx: Context<'_>) -> Result<(), Error> {
 
     let channel = ctx.channel_id();
     let userphone = ctx.data();
-
-    let linked_channel = userphone.current_calls.get(&channel)
+ 
+    match userphone.current_calls.get(&channel)
         .map(|v| v.linked_channel)
-        .ok_or("You are not currently in a call")?;
+    {
+        Some(linked_channel) => {
+            userphone.current_calls.remove(&linked_channel);
+            userphone.current_calls.remove(&channel);
 
-    userphone.current_calls.remove(&linked_channel);
-    userphone.current_calls.remove(&channel);
+            linked_channel.send_message(
+                ctx.http(), 
+                serenity::CreateMessage::new()
+                    .content("The other party has ended the call :( ...")
+            ).await?;
 
-    linked_channel.send_message(
-        ctx.http(), 
-        serenity::CreateMessage::new()
-            .content("The other party has ended the call :( ...")
-    ).await?;
+        },
+        None => {
+            let mut lock = userphone.wait_line.lock();
+
+            let index = lock
+                .iter()
+                .enumerate()
+                .position(|(_, l)| l.current_channel == channel)
+                .ok_or("You are not currently in a call")?;
+
+            lock.remove(index);
+        }
+    };
+
 
     ctx.send(
         poise::CreateReply::default()
